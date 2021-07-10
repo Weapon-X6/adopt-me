21bdeddcbda3f60ffc65d2b728fe4458b3f23a98
waffe_x20 <argueta_eduardo@yahoo.de>
2021-07-10 16:36:15 -0600

    final tool

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
     //...[1,2,3,4].map((i) => React.createElement("h1", {}, i)),
     React.createElement(Pet, {
-      name: "Luna--",
+      name: "Luna",
       animal: "Dog",
       breed: "Havanese",
     }),
